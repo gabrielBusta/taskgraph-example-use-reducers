b8c78081f18d21160b26f26a62dec1f6a4be19fc
Gabriel Bustamante <gbustamante@mozilla.com>
2023-08-30 17:17:05 -0500

    commit

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Sigma from "sigma";
 import { Coordinates, EdgeDisplayData, NodeDisplayData } from "sigma/types";
 import { DirectedGraph as Graph } from "graphology";
 // import data from "./data/firefox-translations-training-ru-en-serialized-taskgraph-multipartite-layout-horizontal.json";
-import data from "./data/ser-ff-release-kinds.json";
+import data from "./data/output/ser-ff-release-kinds.json";
 
 // Retrieve some useful DOM elements:
 const container = document.getElementById("sigma-container") as HTMLElement;
--- serialize_taskgraph.py
+++ serialize_taskgraph.py
@@ -1,21 +1,21 @@
+import argparse
 import json
+from pathlib import Path
 import networkx as nx
 
-TASKGRAPH_NAME = "firefox-translations-training-ru-en"
-LAYOUT_NAME = "multipartite-layout"
-LAYOUT_ALIGNMENT = "vertical"
+# TASKGRAPH_NAME = "firefox-translations-training-ru-en"
+DEFAULT_LAYOUT = "multipartite-layout"
+DEFAULT_ALIGNMENT = "vertical"
 
-
-def load_taskgraph():
-    f = open(f"./data/{TASKGRAPH_NAME}-taskgraph.json")
-    data = f.read()
-    f.close()
+def load_taskgraph(filename):
+    with open(filename) as f:
+        data = f.read()
     taskgraph = json.loads(data)
     return taskgraph
 
 
-def build_digraph(taskgraph):
-    digraph = nx.DiGraph(name=TASKGRAPH_NAME)
+def build_digraph(taskgraph, name):
+    digraph = nx.DiGraph(name=name)
     for tasknode in taskgraph:
         digraph.add_node(tasknode, **taskgraph[tasknode])
         if taskgraph[tasknode]["dependencies"]:
@@ -23,13 +23,12 @@ def build_digraph(taskgraph):
                 digraph.add_edge(
                     taskgraph[tasknode]["dependencies"][dependency], tasknode
                 )
-    print("graph_name:", digraph.name)
-    print("graph_order:", f"{digraph.order():,}")
-    print("graph_size:", f"{digraph.size():,}")
+    print("Order:", f"{digraph.order():,}")
+    print("Size:", f"{digraph.size():,}")
     return digraph
 
 def build_kinds_digraph():
-    with open('./output/ff-release-kinds.json') as f:
+    with open("./output/ff-release-kinds.json") as f:
         data = f.read()
     links = json.loads(data)
     digraph = nx.DiGraph(name="links")
@@ -37,12 +36,9 @@ def build_kinds_digraph():
         digraph.add_node(link)
         if links[link]:
             for dependency in links[link]:
-                digraph.add_edge(
-                    dependency, link
-                )
-    print("graph_name:", digraph.name)
-    print("graph_order:", f"{digraph.order():,}")
-    print("graph_size:", f"{digraph.size():,}")
+                digraph.add_edge(dependency, link)
+    print("Order:", f"{digraph.order():,}")
+    print("Size:", f"{digraph.size():,}")
     return digraph
 
 
@@ -73,7 +69,7 @@ def serialize_kinds(kinds, pos):
                         "target": kind,
                         "attributes": {
                             # "size": 2.5,
-                            "size": .25,
+                            "size": 0.25,
                             "type": "arrow",
                             "kind": kind,
                         },
@@ -83,14 +79,14 @@ def serialize_kinds(kinds, pos):
     return serialized
 
 
-def layout_digraph(digraph):
+def layout_digraph(digraph, alignment):
     for layer, nodes in enumerate(nx.topological_generations(digraph)):
         # `multipartite_layout` expects the layer as a node attribute, so add the
         # numeric layer value as a node attribute
         for node in nodes:
             digraph.nodes[node]["layer"] = layer
     # Compute the multipartite_layout using the "layer" node attribute
-    pos = nx.multipartite_layout(digraph, subset_key="layer", align=LAYOUT_ALIGNMENT)
+    pos = nx.multipartite_layout(digraph, subset_key="layer", align=alignment)
     return pos
 
 
@@ -130,17 +126,40 @@ def serialize_taskgraph(taskgraph, pos):
     return serialized
 
 
-def main():
-    taskgraph = load_taskgraph()
+def main(args):
+    assert args.input
+    layout = args.layout if args.layout else DEFAULT_LAYOUT
+    alignment = args.alignment if args.alignment else DEFAULT_ALIGNMENT
+    print(f"Input: {args.input}")
+    print(f"Layout: {layout}")
+    print(f"Alignment: {alignment}")
+    name = Path(args.input).stem
+    print(f"Name: {name}")
+    output = args.output if args.output else f"./data/output/{name}-{layout}-{alignment}.json"
+    print(f"Output: {output}")
+    taskgraph = load_taskgraph(args.input)
     # digraph = build_kinds_digraph()
-    digraph = build_digraph(taskgraph)
-    pos = layout_digraph(digraph)
+    digraph = build_digraph(taskgraph, name)
+    pos = layout_digraph(digraph, alignment)
     # serialized_kinds = serialize_kinds(digraph, pos)
     # with open(f"./output-serialized-ff-release-kinds.json", "w") as f:
     #     f.write(json.dumps(serialized_kinds, indent=2))
     serialized_taskgraph = serialize_taskgraph(taskgraph, pos)
-    with open(f"./output/{TASKGRAPH_NAME}-serialized-taskgraph-{LAYOUT_NAME}-{LAYOUT_ALIGNMENT}.json", "w") as f:
+    with open(
+        output,
+        "w",
+    ) as f:
         f.write(json.dumps(serialized_taskgraph, indent=2))
 
 
-__name__ == "__main__" and main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simple CLI tool.")
+    parser.add_argument(
+        "-i", "--input", help="Path to the input file.", type=str, required=True
+    )
+    parser.add_argument("-l", "--layout", help="Specify the layout.", type=str)
+    parser.add_argument("-a", "--alignment", help="Specify the alignment.", type=str)
+    parser.add_argument("-o", "--output", help="Path to the output file.", type=str)
+
+    args = parser.parse_args()
+    main(args)
